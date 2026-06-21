a6cfb33a7cd107b3a9e32a1ef7f64f059a762502
teja@bavoltra.com <saiteja@example.com>
2026-06-21 19:03:09 -0400

    deployment changes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+
 
 import Sidebar from "./Sidebar";
 import Header from "./Header";
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Button from "@/components/ui/Button";
 import { useDevice } from "@/hooks/useDevice";
 import { api } from "@/lib/api";
